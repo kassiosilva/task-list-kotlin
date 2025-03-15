7621c85b672ab7809fc7803999f0aef5adad3e82
Kássio Silva <kassio.slva@gmail.com>
2025-03-15 13:23:48 -0300

    feat: add data class Task

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,30 @@
+import java.util.Date
+import java.util.UUID
+
+/**
+ * 1. Criar classe de dados ✅
+ * 2. Utilize companion object para gerar IDs únicos automaticamente para cada Task ✅
+ *
+ * **/
+
+
+data class Task(
+    val id: String = generateId(),
+    val title: String,
+    val description: String? = null,
+    val isCompleted: Boolean,
+    var createdAt: Date = Date()
+) {
+    companion object {
+        private fun generateId() = UUID.randomUUID().toString()
+    }
+}
+
+
 fun main() {
     println("Hello World!")
+
+    println(Task(title = "Teste 1", isCompleted = false))
+    println(Task(title = "Teste 2", isCompleted = true))
+
 }
